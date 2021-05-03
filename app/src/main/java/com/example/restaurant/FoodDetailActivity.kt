a1679825ccc8a9f946b0778e0ce6fb62cc040bf3
Tamires Pereira <tamirespereira6@gmail.com>
2021-05-03 20:03:34 -0300

    Screens

---
+++ app/src/main/java/com/example/restaurant/FoodDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.example.restaurant
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FoodDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_food_detail)
+    }
+}
